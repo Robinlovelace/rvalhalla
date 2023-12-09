314e1cdecf8779a4ad44ad783554324a11c5f665
robinlovelace <rob00x@gmail.com>
2023-12-09 13:16:25 +0000

    Add vh_route()

--- R/vh_get.R
+++ R/vh_get.R
@@ -1,12 +1,46 @@
 #' Get optimized route from Valhalla API
 #'
+#' @param url A string specifying the URL of the Valhalla API. Default is "http://localhost:8002/optimized_route".
+#' @param params Parameters to pass to the Valhalla API as a json object. Default is list().
+#' @param resource A string specifying the resource to request from the Valhalla API. Default is "route". Options are:
+#' * "route": Guides you between points by car, bike, foot, and multimodal combinations involving walking and riding public transit. Your apps can use the results from the route service to plan multimodal journeys with narratives to guide users by text and by voice. Valhalla draws data from OpenStreetMap for the main graph and from user-supplied GTFS feeds for multimodal routing.
+#' * "optimized_route": Computes the times and distances between many origins and destinations and provides you with an optimized path between the locations.
+#' * "matrix": Provides a table of the times and distances between points.
+#' * "isochrone": Computes areas that are reachable within specified time periods from a location or set of locations.
+#' * "map-matching": Matches coordinates to known roads so you can turn a path into a route with narrative instructions and get the attribute values from that matched line.
+#' * "elevation": Finds the elevation along a path or at specified locations.
+#' * "expansion": Returns a geojson representation of a graph traversal at a given location.
+#' * "locate": Provides detailed metadata about the nodes and edges in the graph.
+#' * "status": Returns information about the running server or Valhalla instance.
+#' * "centroid": Finds the least cost convergence point of routes from multiple locations. 
+#' @param ... Additional parameters to pass to the Valhalla API.
+#' @return An sf object representing the optimized route.
+#' @export
+vh_get = function(url = "http://localhost:8002", resource = "route", params = list(), ...) {
+  json = httr2::request(url) |> 
+    httr2::req_url_path_append(resource) |> 
+    httr2::req_body_json(params) |> 
+    # req_user_agent("my_package_name (http://my.package.web.site)") |> 
+    httr2::req_perform() |> 
+    httr2::resp_body_json()
+  json
+}
+
+vh_sfc = function(json) {
+  line = googlePolylines::decode(json)
+  line_sf = sfheaders::sf_linestring(line[[1]])
+  line_sf
+}
+
+#' Get route from Valhalla API
+#' 
 #' @param from A numeric vector of length 2, specifying the longitude and latitude of the starting point.
 #' @param to A numeric vector of length 2, specifying the longitude and latitude of the destination.
 #' @param costing A string specifying the costing model to use for route optimization. Default is "auto".
-#' @param units A string specifying the units to use for the route distance. Default is "miles".
-#' @param url A string specifying the URL of the Valhalla API. Default is "http://localhost:8002/optimized_route".
-#' @param ... Additional arguments to pass to httr2::GET.
-#' @return An sf object representing the optimized route.
+#' @param directions_options A named list.
+#'   By default this is list(units = "km") specifying the units to use.
+#' @inheritParams vh_get
+#' @return An sf object representing the route.
 #' @export
 #' @examples
 #' if (FALSE) {
@@ -14,28 +48,23 @@
 #'   pas_de_la_casa = c(1.7333, 42.5425)
 #'   r1 = vh_get(from = andorra_la_vella, to = pas_de_la_casa)
 #' }
-vh_get = function(from, to, costing = "pedestrian", units = "miles", url = "http://localhost:8002", ..., resource = "optimized_route") {
+vh_route = function(
+    from,
+    to,
+    costing = "pedestrian",
+    directions_options = list(units = "km"),
+    url = "http://localhost:8002",
+    ...
+    ) {
   params = list(
     locations = list(
-        list(lon = from[1], lat = from[2]),
-        list(lon = to[1], lat = to[2])
+      list(lon = from[1], lat = from[2]),
+      list(lon = to[1], lat = to[2])
       ),
-      costing = costing,
-      ...
+    costing = costing,
+    directions_options = directions_options,
+    ...
     )
-#   names(params) = paste0("_", names(params))
-  
-  json = httr2::request(url) |> 
-    httr2::req_url_path_append(resource) |> 
-    httr2::req_body_json(params) |> 
-    # req_user_agent("my_package_name (http://my.package.web.site)") |> 
-    httr2::req_perform() |> 
-    httr2::resp_body_json()
+  json = vh_get(from, to, costing, units, url = url, params = params, resource = "route")
   vh_sfc(json$trip$legs[[1]]$shape)
-}
-
-vh_sfc = function(json) {
-  line = googlePolylines::decode(json)
-  line_sf = sfheaders::sf_linestring(line[[1]])
-  line_sf
 }
--- README.md
+++ README.md
@@ -40,33 +40,15 @@ pas_de_la_casa = c(1.7333, 42.5425)
 
 ``` r
 # Calculate the route
-route1 = vh_get(andorra_la_vella, pas_de_la_casa, costing = "pedestrian")
-route2 = vh_get(andorra_la_vella, pas_de_la_casafrom = andorra_la_vella, to = pas_de_la_casa, costing = "bicycle")
-route3 = vh_get(from = andorra_la_vella, to = pas_de_la_casa, costing = "bicycle", costing_options = list(bicycle = list(use_roads = 0.9)))
+route1 = vh_route(andorra_la_vella, pas_de_la_casa, costing = "pedestrian")
+route2 = vh_route(andorra_la_vella, pas_de_la_casafrom = andorra_la_vella, to = pas_de_la_casa, costing = "bicycle")
+route3 = vh_route(from = andorra_la_vella, to = pas_de_la_casa, costing = "bicycle", costing_options = list(bicycle = list(use_roads = 0.9)))
 ```
 
 We can compare them with `{waldo}`:
 
 ``` r
-waldo::compare(route2, route3)
-#> `attr(old$geometry, 'bbox')`: 425.05 15.22 425.44 17.33
-#> `attr(new$geometry, 'bbox')`: 425.07 15.22 425.84 17.33
-#> 
-#> `dim(old$geometry[[1]])`: 1791 2
-#> `dim(new$geometry[[1]])`: 1686 2
-#> 
-#>       old$geometry[[1]] | new$geometry[[1]]                  
-#> [192] 425.092254638672  | 425.092254638672  [192]            
-#> [193] 425.092620849609  | 425.092620849609  [193]            
-#> [194] 425.092681884766  | 425.092681884766  [194]            
-#> [195] 425.092376708984  - 425.093963623047  [195]            
-#> [196] 425.091979980469  - 425.094512939453  [196]            
-#> [197] 425.091674804688  - 425.096313476562  [197]            
-#> [198] 425.092559814453  - 425.097015380859  [198]            
-#> [199] 425.092742919922  - 425.097503662109  [199]            
-#> [200] 425.092956542969  - 425.098236083984  [200]            
-#> [201] 425.092803955078  - 425.098815917969  [201]            
-#>   ... ...                 ...               and 3225 more ...
+# waldo::compare(route2, route3)
 ```
 
 Let’s plot them all with `ggplot2`:
